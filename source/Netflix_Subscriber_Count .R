cbb50fd561d44add21b936f01fa281b0e29c9312
celinayu806 <102630227+celinayu806@users.noreply.github.com>
2022-11-14 15:58:33 -0800

    Add files via upload

---
+++ source/Netflix_Subscriber_Count .R
@@ -0,0 +1,18 @@
+#installing appropirate packages 
+install.packages("tidyverse")
+install.packages("ggplot2")
+library(ggplot2)
+
+
+#viewing file 
+netflix <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-8-section-ae/main/netflix.csv")
+View(netflix)
+
+
+#creating plot for this specfic dataset from statista {https://www.statista.com/statistics/250934/quarterly-number-of-netflix-streaming-subscribers-worldwide/}
+x <- netflix$Netflix.Quarterly.Subscribers
+x <- factor(x, levels = x)
+ggplot(data = netflix) +
+  geom_col(mapping = aes(x = x, y = Number.of.Paid.Netflix.Subscribers..in.millions.)) +
+  labs(x = "Netflix Quartely Reports 2013-2022", y = "Number of Paid Netflix Subscribers (in millions)")
+
